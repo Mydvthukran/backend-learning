c30a4c87c585ffedd10043b2d3388c231c55cb34
Manish Yadav <my3596418@gmail.com>
2026-07-10 14:09:14 +0530

    feat: implement POST endpoint for notes creation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
-const note = []
+app.use(express.json());
+const notes = []
 
+app.post('/notes' , (req,res) =>{
+    
+notes.push(req.body);
+res.status(201).json({
+    message: "Notes created successfully",
+});
+})
 module.exports = app;
